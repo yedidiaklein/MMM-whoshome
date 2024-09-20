1778b12f7485cb198978ddb027a43f1c2812c932
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2024-09-20 20:35:51 +0200

    Format

--- MMM-whoshome.js
+++ MMM-whoshome.js
@@ -1,61 +1,61 @@
 Module.register("MMM-whoshome", {
     // Define required scripts.
-	getStyles: function () {
-		return ["whoshome.css"];
-	},
-
+    getStyles () {
+      return ["whoshome.css"];
+    },
+  
     // Override dom generator.
-    getDom: function () {
-        var wrapper = document.createElement("div");
-        var i = 0;
-        wrapper.className = "whoshome";
-        var html = '<span class="whoshome-title">Who\'s Home:</span><br><hr>';
-        console.log("in getDom function");
-        console.log(this.peopleArray);
-
-        if (this.peopleArray[0].subject == 'LOADING_ENTRIES') {
-            html = html + '<span class="status">Loading...</span><br>';
-            wrapper.innerHTML = html;
-            return wrapper;
-        }
-        for( i = 0; i < this.peopleArray.length; i++ ) {
-            if (this.peopleArray[i][1] == 1) {
-                html = html + '<img class="person" src="' + this.config.TRACK[this.peopleArray[i][0]].image + '"> ' + this.peopleArray[i][0] + '<BR>';
-            } else {
-                html = html + '<img class="person bw-image" src="' + this.config.TRACK[this.peopleArray[i][0]].image 
-                    + '"> <span class="person-away">' + this.peopleArray[i][0] + '</span><BR>'
-                    + '<span class="lastseen">Last seen: ' + this.peopleArray[i][2] + '</span><BR>';
-            }
-        }
-
+    getDom () {
+      const wrapper = document.createElement("div");
+      wrapper.className = "whoshome";
+      let html = "<span class='whoshome-title'>Who's Home:</span><br><hr>";
+      Log.log("in getDom function");
+      Log.log(this.peopleArray);
+  
+      if (this.peopleArray[0].subject === "LOADING_ENTRIES") {
+        html = `${html}<span class="status">Loading...</span><br>`;
         wrapper.innerHTML = html;
         return wrapper;
-    },
-
-    socketNotificationReceived: function(notification, payload) {
-        console.log("socketNotificationReceived: " + notification);
-        if (payload.id == this.identifier) {
-            switch(notification) {
-                case "FETCHED_PEOPLE":
-                  this.peopleArray = payload.people;
-                  this.updateDom();
-                  break
-              }
+      }
+      for (let index = 0; index < this.peopleArray.length; index++) {
+        if (this.peopleArray[index][1] === 1) {
+          html = `${html}<img class="person" src="${this.config.TRACK[this.peopleArray[index][0]].image}"> ${this.peopleArray[index][0]}<BR>`;
+        } else {
+          html = `${html}<img class="person bw-image" src="${this.config.TRACK[this.peopleArray[index][0]].image
+          }"> <span class="person-away">${this.peopleArray[index][0]}</span><BR>` +
+          `<span class="lastseen">Last seen: ${this.peopleArray[index][2]}</span><BR>`;
         }
+      }
+  
+      wrapper.innerHTML = html;
+      return wrapper;
     },
-
-    start: function () {
-        var self = this
-
-        self.peopleArray = [{ subject: 'LOADING_ENTRIES' }]
-
-        // update tasks every 60s
-        var refreshFunction = function () {
-            self.config['id'] = self.identifier;
-            self.sendSocketNotification('FETCH_PEOPLE', self.config)
+  
+    socketNotificationReceived (notification, payload) {
+      Log.debug(`socketNotificationReceived: ${notification}`);
+      if (payload.id === this.identifier) {
+        switch (notification) {
+          case "FETCHED_PEOPLE":
+            this.peopleArray = payload.people;
+            this.updateDom();
+            break;
         }
-        refreshFunction()
-        setInterval(refreshFunction, 60000)
+      }
+    },
+  
+    start () {
+      const self = this;
+  
+      self.peopleArray = [{subject: "LOADING_ENTRIES"}];
+  
+      // update tasks every 60s
+      const refreshFunction = function () {
+        self.config.id = self.identifier;
+        self.sendSocketNotification("FETCH_PEOPLE", self.config);
+      };
+      refreshFunction();
+      setInterval(refreshFunction, 60000);
     }
-
-});
+  
+  });
+  
